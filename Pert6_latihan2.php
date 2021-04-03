84a81c1e3ae28a80f6d0c6790be6a1d425d65ffd
Antoniussibarani23 <76987351+Antoniussibarani23@users.noreply.github.com>
2021-04-03 19:28:14 +0700

    Add files via upload

---
+++ Pert6_latihan2.php
@@ -0,0 +1,11 @@
+<html> 
+<body> 
+<?php 
+$x=array("blue","yellow","orange"); 
+foreach ($x as $value) 
+ { 
+ echo $value . "<br />"; 
+ } 
+?> 
+</body> 
+</html>
---
+++ Pert6_latihan3.php
@@ -0,0 +1,11 @@
+<?php
+$UsiaKaryawan["Roy"] = "18"; 
+$UsiaKaryawan["Gabi"] = "26"; 
+$UsiaKaryawan["Son"] = "25"; 
+$UsiaKaryawan["Los"] = "46"; 
+$UsiaKaryawan["Gos"] = "31"; 
+foreach($UsiaKaryawan as $Nama => $umur) 
+{ 
+echo "Nama Karyawan: $Nama, Usia: $umur"." th <br>"; 
+} 
+?>
